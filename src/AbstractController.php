1f681405cf55cf71f04ee315e918a30c27563ae7
william machi <megabillou@gmail.com>
2020-06-24 09:51:16 +0200

    AbstractController Update

--- src/AbstractController.php
+++ src/AbstractController.php
@@ -6,6 +6,11 @@ abstract class AbstractController
 {
 	private $templateEngine;
 
+	private $_session;
+
+    private $_flashbag;
+
+
 	public function __construct()
 	{
 		$loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__, 4) . '\templates');
@@ -17,4 +22,27 @@ abstract class AbstractController
 		//echo "$view.html.twig";
 		return $this->templateEngine->render($view.'.html.twig', $vars);
 	}
+
+	protected function session()
+	{
+		if($this->_session == null)
+		{
+			$this->_session = new Session();
+		}
+		return $this->_session;
+	}
+
+	protected function flashbag()
+    {
+        if($this->_flashbag === null) {
+            $this->_flashbag = new FlashBag();
+        }
+        return $this->_flashbag;
+    }
+
+    protected function redirectToRoute(string $url)
+    {
+        header("location:".$url);
+        exit();
+    }
 }
